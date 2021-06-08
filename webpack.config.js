0f00201636bb0bf4d98d52436b59e41107cfacb5
devagiloscarmesa <oscar.mesa@agileinnova.org>
2021-06-08 13:06:45 -0500

    cambios

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,36 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-    
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'development',
-  entry: './src/app/index.js',
-  output: {
-    filename: 'app.bundle.js',
-    path: path.resolve(__dirname, 'build'),
-  },
+  entry: { main: './src/app/index.js', somos: './src/app/somos.js' },
+ 
   plugins: [
-    new HTMLWebpackPlugin({template: './src/index.html'})
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: false,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+      chunks: ['main']
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'somos.html',
+      template: './src/somos.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: false,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+      chunks: ['somos']
+    })
   ]
-
-};
+}
